832f5923284b0bf449ddba763e6dcac636b1a019
Aleksandar Cvetković <office@e-seo.info>
2025-09-02 20:53:56 +0200

    chore: setup development tools (Husky, Prettier, ESLint)

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -20,4 +22,14 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+  prettierConfig,
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]);
